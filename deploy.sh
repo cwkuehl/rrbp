fb221e98941444c2d9ebd86af20a2e6ed9909198
wolfgang <wolfgang@cwkuehl.de>
2023-10-01 19:57:26 +0200

    Start script added.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 cargo build -r
 # /opt/Haushalt/rrbp/Rocket.toml
 cp -f ~/rust/rrbp/target/release/rrbp /opt/Haushalt/rrbp
+cp -f ~/rust/rrbp/#rrbp.sh /opt/Haushalt/rrbp
 cp -rf ~/rust/rrbp/templates /opt/Haushalt/rrbp
 #cp -f /opt/Haushalt/CSBP/cert/cert.pem /opt/Haushalt/CSBP/cert/cert.key /opt/Haushalt/rrbp/cert
 # ~/hsqldb/rsbp.db
-
